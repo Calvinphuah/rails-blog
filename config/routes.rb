192055a28ab8497531e37490a663f80fc9172735
calvin phuah <calvinphuah73@gmail.com>
2025-09-21 16:26:39 +0800

    feat: index, new, show actions

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+
+  root "blog_posts#index"
 end
